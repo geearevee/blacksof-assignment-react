72b079897dc6b7af20cc73df5b7f7ebb510e5339
geearevee <110712685+geearevee@users.noreply.github.com>
2023-09-28 18:53:46 +0530

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,28 @@ import footerLogo from "../assets/review-logo.svg";
 const Footer = () => {
   return (
     <div>
-      <p className="text-center text-gray-100 mt-20">
+      <p className="text-center text-gray-100 mt-[200px] mb-4">
         Leading the charge for Industries!
       </p>
       <div className="flex justify-center gap-3 mt-4 overflow-hidden mb-4">
+        <button className="bg-gray-300 px-4 flex-shrink-0 py-2 rounded-md transition-colors duration-300 ease-in-out hover:bg-white">
+          <img src={footerLogo} alt="footer logo" />
+        </button>
+        <button className="bg-gray-300 px-4 flex-shrink-0 py-2 rounded-md transition-colors duration-300 ease-in-out hover:bg-white">
+          <img src={footerLogo} alt="footer logo" />
+        </button>
+        <button className="bg-gray-300 px-4 flex-shrink-0 py-2 rounded-md transition-colors duration-300 ease-in-out hover:bg-white">
+          <img src={footerLogo} alt="footer logo" />
+        </button>
+        <button className="bg-gray-300 px-4 flex-shrink-0 py-2 rounded-md transition-colors duration-300 ease-in-out hover:bg-white">
+          <img src={footerLogo} alt="footer logo" />
+        </button>
+        <button className="bg-gray-300 px-4 flex-shrink-0 py-2 rounded-md transition-colors duration-300 ease-in-out hover:bg-white">
+          <img src={footerLogo} alt="footer logo" />
+        </button>
+        <button className="bg-gray-300 px-4 flex-shrink-0 py-2 rounded-md transition-colors duration-300 ease-in-out hover:bg-white">
+          <img src={footerLogo} alt="footer logo" />
+        </button>
         <button className="bg-gray-300 px-4 flex-shrink-0 py-2 rounded-md transition-colors duration-300 ease-in-out hover:bg-white">
           <img src={footerLogo} alt="footer logo" />
         </button>
